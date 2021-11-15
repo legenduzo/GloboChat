6bf756bd31c3b1ac7a0ce316784280e8bcdb2403
legenduzo <precious@evolvedigital.org>
2021-11-15 23:01:59 +0100

    Further work on settings logic

--- app/src/main/java/org/evolvedigital/globochat/MainActivity.kt
+++ app/src/main/java/org/evolvedigital/globochat/MainActivity.kt
@@ -1,16 +1,19 @@
 package org.evolvedigital.globochat
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import org.evolvedigital.globochat.R
+import androidx.preference.PreferenceManager
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
 
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -19,18 +22,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        // Get NavHost and NavController
-//        val navHostFrag = supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
-//        navController = navHostFrag.navController
-//
-//        // Get AppBarConfiguration
-//        appBarConfiguration = AppBarConfiguration(navController.graph)
-//
-//        // Link ActionBar with NavController
-//        setupActionBarWithNavController(navController, appBarConfiguration)
+        // Get NavHost and NavController
+        val navHostFrag = supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
+        navController = navHostFrag.navController
+
+        // Get AppBarConfiguration
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        // Link ActionBar with NavController
+       setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // Step 1: Get reference to the SharedPreference (XML file)
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        // Step 2: Get the 'value' using the 'key'
+        val autoReplyTime = sharedPreferences.getString(getString(R.string.key_auto_reply_time), "")
+        Log.i("MainActivity", "Auto Reply Time: $autoReplyTime")
     }
 
-//    override fun onSupportNavigateUp(): Boolean {
-//        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-//    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    // Called only 'after' the Preference value has changed
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        if (key == "key_status")  {
+            val newStatus = sharedPreferences?.getString(key, "")
+            Toast.makeText(this, "New Status: $newStatus", Toast.LENGTH_SHORT).show()
+        }
+
+        if (key == "key_auto_reply") {
+            val autoReply = sharedPreferences?.getBoolean(key, false)
+            if (autoReply!!) {
+                Toast.makeText(this, "Auto Reply: ON", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Auto Reply: OFF", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this)
+    }
 }
+
--- app/src/main/java/org/evolvedigital/globochat/SettingsFragment.kt
+++ app/src/main/java/org/evolvedigital/globochat/SettingsFragment.kt
@@ -1,28 +1,55 @@
 package org.evolvedigital.globochat
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.widget.Toast
 import androidx.navigation.fragment.NavHostFragment
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
 
-        view.setOnClickListener {
+        val accSettingsPref = findPreference<Preference>(getString(R.string.key_account_settings))
+
+        accSettingsPref?.setOnPreferenceClickListener {
             val navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_frag) as NavHostFragment
             val navController = navHostFragment.navController
             val action = SettingsFragmentDirections.actionSettingsToAccSettings()
             navController.navigate(action)
+            true
         }
 
-        return view
+        // Read Preference values in a fragment
+        // Step 1: Get reference to the SharedPreference (XML file)
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+        // Step 2: Get the 'value' using the 'key'
+        val autoReplyTime = sharedPreferences.getString(getString(R.string.key_auto_reply_time), "")
+        Log.i("SettingsFragment", "Auto Reply Time: $autoReplyTime")
+
+        val autoDownload = sharedPreferences.getBoolean(getString(R.string.key_auto_download), false)
+        Log.i("SettingsFragment", "Auto Download: $autoDownload")
+
+        val statusPref = findPreference<EditTextPreference>(getString(R.string.key_status))
+        statusPref?.setOnPreferenceChangeListener { _, newValue ->
+
+            val newStatus = newValue as String
+            if (newStatus.contains("bad")) {
+                Toast.makeText(context, "Inappropriate status. Please maintain community guidelines",
+                    Toast.LENGTH_SHORT).show()
+                false //false: reject the vale
+            } else {
+                Log.i("SettingsFragment", "New status: $newValue")
+
+                true // true: accept the new value.
+            }
+        }
     }
+
+
 }
